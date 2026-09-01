bc5cdbf81611c900f98a3feff4d01d93c46b40f7
Masooma-Ali <masoomaali347@gmail.com>
2026-09-01 20:19:01 +0500

    Stage 1: root and health endpoints

--- server.js
+++ server.js
@@ -1,10 +1,35 @@
-// server.js
+// stage 0
+/*
 const express = require("express");
 const app = express();
 const PORT = 3000;
 
 app.get("/", (req, res) => {
   res.send("Hello, world!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
+*/
+
+
+//STAGE 1
+// server.js
+const express = require("express");
+const app = express();
+const PORT = 3000;
+
+app.get("/", (req, res) => {
+  res.json({
+    name: "Task API",
+    version: "1.0",
+    endpoints: ["/tasks"],
+  });
+});
+
+app.get("/health", (req, res) => {
+  res.json({ status: "ok" });
 });
 
 app.listen(PORT, () => {
